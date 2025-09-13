84f1d93b74a5f82cca8e962b04a62679a4653597
PMurphyB <pmurphyguitar@gmail.com>
2025-09-13 16:44:50 -0600

    Commented out smoothing

--- track.m
+++ track.m
@@ -18,5 +18,13 @@ xlabel("X");, ylabel("Y");, title("Track Map"), grid on;
 xlim([min(X) - 25,max(X) + 25]), ylim([min(Y) - 25,max(Y) + 25]);
 axis equal;
 
-scatter(X, Y, 'filled');
-axis equal, grid on;
+
+
+%xx = linspace(min(X), max(X), 500);
+%yy = spline(X, Y, xx);
+%hold on;
+%plot(xx, yy, 'r-', 'LineWidth', 2);
+%legend('Control Points', 'Spline Fit');
+
+% scatter(X, Y, 'filled');
+% axis equal, grid on;
