88a4bc3699674ae8e21da595cea46b14244751f3
PMurphyB <pmurphyguitar@gmail.com>
2025-09-20 00:20:47 -0600

    Updated smoothness

--- track.m
+++ track.m
@@ -12,8 +12,13 @@ invalidMask = (X == 0 | Y == 0 | X == 1e20 | Y == 1e20 | X == -1e20 | Y == -1e20
 X(invalidMask) = [];
 Y(invalidMask) = [];
 
+t = 1:length(X);
+tt = linspace(1, length(X), 2000);
+xx = interp1(t, X, tt, 'pchip');
+yy = interp1(t, Y, tt, 'pchip');
+
 figure(1);
-plot(X, Y, 'g');
+plot(xx, yy, 'g');
 hold on;
 xlabel("X");, ylabel("Y");, title("Track Map"), grid on;
 xlim([min(X) - 25,max(X) + 25]), ylim([min(Y) - 25,max(Y) + 25]);
@@ -22,16 +27,7 @@ axis equal;
 tracer = plot(X(1), Y(1), 'bo', 'MarkerSize', 8, 'MarkerFaceColor', 'b');
 
 for i = 1:length(X)
-    set(tracer, 'XData', X(i), 'YData', Y(i));
+    set(tracer, 'XData', xx(i), 'YData', yy(i));
     drawnow;
     pause(0.1); % smaller pause = faster animation
 end
-
-%xx = linspace(min(X), max(X), 500);
-%yy = spline(X, Y, xx);
-%hold on;
-%plot(xx, yy, 'r-', 'LineWidth', 2);
-%legend('Control Points', 'Spline Fit');
-
-% scatter(X, Y, 'filled');
-% axis equal, grid on;
